a074bdc6944237adf94d20700a812142965586e0
LucioFider <fidelislucio404@gmail.com>
2023-02-16 07:50:47 +1000

    4th-commit

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Navbar from "./navbar/Navbar";
+import Hero from "./hero/Hero";
+import ProductCard from "./productCard/ProductCard";
+
+export { Navbar, Hero, ProductCard };
---
+++ src/components/productCard/ProductCard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./productCard.scss";
+
+const ProductCard = () => {
+  return <div className="productCard">ProductCard</div>;
+};
+
+export default ProductCard;
