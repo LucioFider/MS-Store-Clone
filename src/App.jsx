8bde2879a3d0e557acf0aed1381fea245e28c32f
LucioFider <fidelislucio404@gmail.com>
2023-02-16 08:21:17 +1000

    7th-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import "./app.scss";
-import { createBrowserRouter, Outlet, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <div>
+          <h1>Hello World</h1>
+          <Link to="about">About Us</Link>
+        </div>
+      ),
+    },
+    {
+      path: "about",
+      element: <div>About</div>,
+    },
+  ]);
+
   return (
     <div className="app">
       <h1>Ms Store Clone</h1>
