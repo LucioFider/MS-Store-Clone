08d4ba53bb758498e884beee4ad2fddc28a1cea1
LucioFider <fidelislucio404@gmail.com>
2023-02-16 07:43:46 +1000

    4th-commit

---
+++ src/constants/index.js
@@ -0,0 +1,59 @@
+import {
+  avatar,
+  featImg,
+  free,
+  freeman,
+  prod01,
+  prod02,
+  prod03,
+  prod04,
+  prod05,
+  prod06,
+  slide01,
+  slide02,
+  slide03,
+  feat01,
+  feat02,
+  feat03,
+  feat04,
+  feat05,
+} from "../assets";
+
+export const data = [
+  {
+    id: 1,
+    img: prod01,
+    title: "Skirt",
+    price: 56.99,
+  },
+  {
+    id: 2,
+    img: prod02,
+    title: "Skirt",
+    price: 36.99,
+  },
+  {
+    id: 3,
+    img: prod03,
+    title: "Skirt",
+    price: 50.99,
+  },
+  {
+    id: 4,
+    img: prod04,
+    title: "Skirt",
+    price: 50.99,
+  },
+  {
+    id: 5,
+    img: prod05,
+    title: "Skirt",
+    price: 50.99,
+  },
+  {
+    id: 6,
+    img: prod06,
+    title: "Skirt",
+    price: 60.99,
+  },
+];
