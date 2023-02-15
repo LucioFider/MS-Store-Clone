ee2bdec257856a595d84e1fb4bf0a22d05c72451
LucioFider <fidelislucio404@gmail.com>
2023-02-16 07:29:29 +1000

    2nd-commit

---
+++ src/assets/index.js
@@ -0,0 +1,29 @@
+import avatar from "./avatar.jpg";
+import featImg from "./feat-img.png";
+import free from "./free.png";
+import freeman from "./freeman.png";
+import prod01 from "./prod01.png";
+import prod02 from "./prod02.png";
+import prod03 from "./prod03.png";
+import prod04 from "./prod04.png";
+import prod05 from "./prod05.png";
+import prod06 from "./prod06.png";
+import slide01 from "./slide1.png";
+import slide02 from "./slide2.png";
+import slide03 from "./slide3.png";
+
+export {
+  avatar,
+  featImg,
+  free,
+  freeman,
+  prod01,
+  prod02,
+  prod03,
+  prod04,
+  prod05,
+  prod06,
+  slide01,
+  slide02,
+  slide03,
+};
