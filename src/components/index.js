cf639a970d9e7227f1641813d31f493fa7647b2b
LucioFider <fidelislucio404@gmail.com>
2023-02-16 07:57:49 +1000

    5th-commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Navbar from "./navbar/Navbar";
 import Hero from "./hero/Hero";
 import ProductCard from "./productCard/ProductCard";
+import Button from "./button/Button";
 
-export { Navbar, Hero, ProductCard };
+export { Navbar, Hero, ProductCard,Button };
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from "./home/Home";
+import About from "./about/About";
+import Footer from "./footer/Footer";
+import Categories from "./categories/Categories";
+
+export { Home, About, Footer, Categories };
