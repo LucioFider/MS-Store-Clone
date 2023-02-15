22093ede64c6d4fb3783e05732008b71ef70ec74
LucioFider <fidelislucio404@gmail.com>
2023-02-16 07:22:44 +1000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./app.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Ms Store Clone</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/icons/index.jsx
@@ -0,0 +1,34 @@
+import {
+  AiOutlineHome,
+  AiOutlineShoppingCart,
+  AiOutlineMenu,
+  AiOutlineSearch,
+  AiOutlineUser,
+  AiOutlineAppstoreAdd,
+  AiOutlineHeart,
+} from "react-icons/ai";
+import { IoIosHelpCircleOutline } from "react-icons/io";
+import { BiCategory, BiBookAlt } from "react-icons/bi";
+import { SiYoutubegaming } from "react-icons/si";
+import { FaTshirt, FaUtensils } from "react-icons/fa";
+import { GiArmoredPants, GiSonicShoes } from "react-icons/gi";
+import { MdOutlineHardware } from "react-icons/md";
+
+export {
+  AiOutlineHome,
+  AiOutlineShoppingCart,
+  AiOutlineMenu,
+  AiOutlineSearch,
+  AiOutlineUser,
+  AiOutlineAppstoreAdd,
+  AiOutlineHeart,
+  IoIosHelpCircleOutline,
+  BiCategory,
+  BiBookAlt,
+  SiYoutubegaming,
+  FaTshirt,
+  FaUtensils,
+  GiArmoredPants,
+  GiSonicShoes,
+  MdOutlineHardware,
+};
